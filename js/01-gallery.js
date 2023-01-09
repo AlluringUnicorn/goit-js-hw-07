8fd9d7e34472d4747feb312d1b19a76a67d0c1f9
Mariia Khapun <mariakhapun@gmail.com>
2023-01-09 21:10:41 +0000

    finished hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+// console.log(gallery);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+// console.log(galleryMarkup);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", onGalleryClick);
+
+
+
+function onGalleryClick(event) {
+
+  event.preventDefault();
+
+  const modal = basicLightbox
+    .create(
+      `
+    <img width="1400" height="900" src="${event.target.dataset.source}">
+`
+    )
+    .show();
+
+    document.addEventListener("keydown", (event) => {
+        console.log(event);
+      
+        if (event.code === "Escape") {
+          modal.close(() => document.removeEventListener("keydown"));
+        }
+      });
+}
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const modal = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
